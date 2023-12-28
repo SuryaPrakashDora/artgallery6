27d531d156dda8ac0620abea899e1509a50e32d7
suryaprakashdora <prakashmadakam21@gmail.com>
2023-12-28 17:47:17 +0000

    solution

---
+++ src/main/java/com/example/artgallery/controller/ArtController.java
@@ -0,0 +1,58 @@
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.*;
+ * 
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.artgallery.controller;
+
+import com.example.artgallery.model.Art;
+import com.example.artgallery.model.Artist;
+import com.example.artgallery.service.ArtJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ArtController {
+    @Autowired
+    private ArtJpaService artJpaService;
+
+    @GetMapping("/galleries/artists/arts")
+    public ArrayList<Art> getArts() {
+        return artJpaService.getArts();
+    }
+
+    @GetMapping("/galleries/artists/arts/{artId}")
+    public Art getArtsById(@PathVariable("artId") int artId) {
+        return artJpaService.getArtById(artId);
+    }
+
+    @PostMapping("/galleries/artists/arts")
+    public Art addArt(@RequestBody Art art) {
+        return artJpaService.addArt(art);
+    }
+
+    @PutMapping("/galleries/artists/arts/{artId}")
+    public Art updateArt(@PathVariable("artId") int artId, @RequestBody Art art) {
+        return artJpaService.updateArt(artId, art);
+    }
+
+    @DeleteMapping("/galleries/artists/arts/{artId}")
+    public void deleteArt(@PathVariable("artId") int artId) {
+        artJpaService.deleteArt(artId);
+    }
+
+    @GetMapping("/arts/{artId}/artist")
+    public Artist getArtArtists(@PathVariable("artId") int artId) {
+        return artJpaService.getArtArtists(artId);
+    }
+}
---
+++ src/main/java/com/example/artgallery/repository/ArtJpaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * import org.springframework.stereotype.Repository;
+ * 
+ */
+
+// Write your code here
+package com.example.artgallery.repository;
+
+import com.example.artgallery.model.Art;
+import com.example.artgallery.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ArtJpaRepository extends JpaRepository<Art, Integer> {
+    List<Art> findByArtist(Artist artist);
+}
